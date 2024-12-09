f67ee332d93bc8dd45eb78477564083ac0967244
Ben Williams <codebencode@icloud.com>
2024-12-09 10:11:35 +0000

    Python Comments Variables and Types

---
+++ main.py
@@ -0,0 +1,30 @@
+""" this comments out any code
+"""
+"""variables"""
+my_number = 5
+print(my_number)
+
+"""" function """
+
+def multiplication(num1, num2):
+    return num1 * num2
+
+result1= multiplication(2,3)
+print(result1)
+""" returns 6 - 2 * 3 = 6
+
+""" Determining data types """
+
+numbers, boolean, set, sequence types & Dictionary (data mapping)
+"""
+print(type('hello)) """ prints class 'str' """ for string
+
+print(type(42)) """ prints class int - number'
+
+Print (type(3.145)) = float - number with decimals
+print (type(1j)) prints complex - mix of objects
+print (type([egg, bacon, spam])) prints list
+print(type(egg,bacon,spam)) prints tuple
+print(type({name : john, age : 80})) prints dict
+
+print(isinstance(3.14, int)) = False = as 3.14 is not a integar it is a float
---
+++ script.js
@@ -0,0 +1,15 @@
+// Notes for Python
+
+// """ = 3 """ is comment out in python
+
+//Declaring variables
+
+    my_number = 5
+    print(my_number)
+
+// Functions
+
+def functionn_name():
+    print('hello')
+
+function_name() // runs the function
