d059938d5a935cb74bd05159c6bc4bce62688462
Ben Williams <codebencode@icloud.com>
2024-12-09 11:23:39 +0000

    Add string methods

--- main.py
+++ main.py
@@ -89,3 +89,47 @@ f'{string or expression}'
 name = input('what is your name") - string
 age = input('what is your age) - number
 print (f'hello {name}, you are {age} years old) = adding f means we can print strings and numbers without needing to change them
+
+name = 'Ben'
+age = 35
+print(f "{name} is {age}) = Ben is 35
+
+""" types of string methods"""
+
+capitalize()	Capitalizes the first character of the string
+center()	Centers string
+count()	Returns a count of times a specified value occurs in the string
+encode()	Returns an encoded version of the string (use decode() to decode)
+endswith()	Returns True if the string ends with a specified suffix
+expandtabs()	Sets the tab size in spaces of the string
+find()	Returns the lowest index position of where a specified character was found
+index()	Searches for a specified value and returns the position of where it was found or an error if not found
+isalnum()	Returns True if all characters are alphanumeric
+isalpha()	Returns True if all characters are alphabetic
+isdigit()	Returns True if all characters are digits
+islower()	Returns True if all characters are lower case
+isspace()	Returns True if all characters are whitespace
+istitle()	Returns True if the string is titlecased
+isupper()	Returns True if all characters in the string are upper case
+join()	concatenates string
+ljust()	Returns a left justified version of the string
+lower()	Converts a string into lower case
+lstrip()	Returns a left trim version of the string
+partition()	Returns a tuple where the string is parted into two strings and the separator
+replace()	Returns a string where a old value is replaced with a new value
+rfind()	Searches highest index in the string for a specified value
+rindex()	Same but with error if nothing found
+rjust()	Returns a right justified version of the string
+rpartition()	Returns a tuple where the string is parted into three parts
+rsplit()	Splits the string at the specified separator, and returns a list
+rstrip()	Returns a right trim version of the string
+split()	Splits the string at the specified separator, and returns a list
+splitlines()	Splits the string at line breaks and returns a list
+startswith()	Returns true if the string starts with the specified value
+strip()	Returns a trimmed version of the string
+swapcase()	Swaps cases, lower case becomes upper case and vice versa
+title()	Converts the first character of each word to upper case
+translate()	Returns a translated string
+upper()	Converts a string into uppercase
+zfill()	Fills the string with a specified number of 0 values at the beginning
+
