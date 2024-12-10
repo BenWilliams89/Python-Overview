71b647223432affb1abdfebb910047beab95fd0b
Ben Williams <codebencode@icloud.com>
2024-12-10 11:35:17 +0000

    Add list overview rules

--- main.py
+++ main.py
@@ -366,3 +366,43 @@ while x <= 3:
         print(x, y)
         y += 1
     x += 1
+
+
+""" List rules """
+
+fruits = ['apple', 'orange', 'banana', 'pear', 'plum']
+
+# Print all fruits
+for fruit in fruits:
+    print(fruit)
+
+print()
+
+# Get an item located in a list
+second_item = fruits[1]
+print(second_item)
+print()
+
+# Add an item to the list
+fruits.append('cherries')
+print(fruits)
+print()
+
+# Reverse the list
+fruits.reverse()
+print(fruits)
+
+# Sort the list alphabetically:
+fruits.sort()
+print(fruits)
+
+example of pulling data from a list:
+
+all_numbers = [1, 5, 44, 22, 13, 17, 56, 3, 88, 9, 97]
+big_numbers = []
+for number in all_numbers:
+    if number > 40:
+        big_numbers.append(number)
+
+print(all_numbers)
+print(big_numbers)
