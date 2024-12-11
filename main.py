c361389c4af8bf10ff0b95a8e935d42e5b175f0f
Ben Williams <codebencode@icloud.com>
2024-12-11 15:42:43 +0000

    Add alternative way to get odd numbers for puzzle

--- main.py
+++ main.py
@@ -1113,14 +1113,17 @@ By Code Institute
 
 """ How to return odd numbers from a list"""
 
-numbers = [12, 45, 60, 87, 999, 200, 84, 42, 87, 77, 2, 3, 77, 99, 20]
+numbers = [12, 45, 60, 87, 999, 200, 84, 42, 87, 77, 2, 3, 77, 99, 20, 13]
 
-def get_odd_numbers(list):
+def odd_nums(numbers):
     new_list = []
-    for num in list:
+    for num in numbers:
         if num % 2 != 0:
-            new_list.append(num)
+                new_list.append(num)
     return new_list
-result = get_odd_numbers(numbers)
+    
+result = odd_nums(numbers)
 print(result)
+
+""" Defining classes in Python"""
 
