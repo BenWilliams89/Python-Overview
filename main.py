5959baa064fb59a8bc4bc13f150eee6e772ecf71
Ben Williams <codebencode@icloud.com>
2024-12-11 12:50:18 +0000

    Add *arg & **karg

--- main.py
+++ main.py
@@ -938,3 +938,56 @@ print(total) = 40 (21+19) as these are the only numbers >= 15
 
 """ Splat! *args * **kwargs"""
 
+Allow to pass many arguments
+
+*args = can be renamed at a latter time
+**kwards = similar to *args but is used when a i use a keyword or named argument
+
+def addition(a, b):
+    return a + b
+
+print(addition(2,2))
+
+def add_integers(list_integers):
+	total = 0
+	for x in list_integers:
+		total += x
+	return total
+
+list_integers = [1, 2, 3, 4]
+print(add_integers(list_integers))
+
+def add_many_integers(*integers):
+	# Rename *args to something suitable
+	total = 0
+	for x in integers:
+		# As it is a tuple you can use the in keyword to iterate 
+		total += x
+	return total
+
+print(add_many_integers(1,2,3,4,5,6,7,8,9))
+
+def concatenate_words(**words):
+	result = ""
+	# As **kwargs is a dict you need to iterate over .values()
+	for arg in words.values():
+		result += arg
+		result += " "
+	return result
+
+print(concatenate_words(a='This', b="is", c="a", d="useful", e="feature"))
+
+Another Example:
+
+def make_string(*strings):
+    return ' '.join(strings)
+
+my_string = make_string("Alderaan", "Coruscant", "Dagobah", "Endor", "Hoth")
+print(my_string)
+
+def get_age(**data):
+    return data.get('age', "no age provided")
+
+pats_age = get_age(name="pat", level=4, age=33, occupation="postman")
+print(pats_age)
+
